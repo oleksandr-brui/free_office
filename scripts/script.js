3e1e4d68b03950f133e6fb21181afca576120a9f
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-09-06 16:50:28 +0300

    solution free_office_psd_template

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-'use strict';
+$(document).ready(function(){
+  $('.project-title__images-container').slick({
+    arrows: false,
+    dots: true,
+    swipe: true,
+  });
+});
+
+$(document).ready(function(){
+  $('.slow-life__slider-container').slick({
+    arrows: false,
+    dots: true,
+    swipe: true,
+  });
+});
